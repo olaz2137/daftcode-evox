a94e4836d62b8c97a587ab031af7e9cf43fc9f3a
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-24 12:41:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from app import schemas, models
-from app.database import engine
+
+from .views import router as text_api_router
 
 app = FastAPI()
 
-models.Base.metadata.create_all(engine)
-
-@app.post('/post')
-def create(request: schemas.Text ):
-    return request
+app.include_router(text_api_router, tags=["text"])
